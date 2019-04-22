927c725c20e86596f169096ddd929ef9d7243079
nikhil153 <nikhil153@gmail.com>
2019-04-22 13:15:07 -0400

    python wrapper for aparctablestats in FS

---
+++ scripts/get_roi_data_fs.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# @author Nikhil Bhagawt
+# @date 22 April 2019
+
+import numpy as np
+import argparse
+import csv
+import os 
+import sys
+
+# input parser
+parser = argparse.ArgumentParser(description='Process ROI-wise output from FS using aparcstats2table')
+parser.add_argument('-s','--subjectdir',help='path for the file with subject list from freesurfer output')
+parser.add_argument('-l','--listofsubjects',help='path for the file with subject list from freesurfer output')
+parser.add_argument('-m','--meas',help='phenotypic measure (e.g. thickness, surface area etc.')
+parser.add_argument('-p','--parc',help='cortical surface parcellation (e.g. aparc.Glasseratlas')
+parser.add_argument('-o','--output',help='path for the left and right output CSVs for ROI-wise cortical thickness from freesurfer output')
+
+args = parser.parse_args()
+
+subject_dir = args.subjectdir
+subjectfile = args.listofsubjects
+measure = args.meas
+parc = args.parc
+file_l = args.output + '_' + parc + '_' + measure + '_lh.csv'
+file_r = args.output + '_' + parc + '_' + measure + '_rh.csv'
+
+os.environ['SUBJECTS_DIR'] = subject_dir 
+os.system('module load freesurfer')
+os.system('aparcstats2table --hemi lh --subjectsfile {} --skip --meas {} --parc aparc.{} --tablefile {}'.format(subjectfile,measure,parc,file_l))
+os.system('aparcstats2table --hemi rh --subjectsfile {} --skip --meas {} --parc aparc.{} --tablefile {}'.format(subjectfile,measure,parc,file_r))
