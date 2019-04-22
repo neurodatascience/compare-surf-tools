fd95e82a811f2f98f96dc5e6670ff6ab127483b5
nikhil153 <nikhil153@gmail.com>
2019-04-22 15:32:50 -0400

    minor changes to make it compatible with docker env

--- scripts/get_roi_data_fs.py
+++ scripts/get_roi_data_fs.py
@@ -26,13 +26,13 @@ parc = args.parc
 file_l = args.output + parc + '_' + measure + '_lh.csv'
 file_r = args.output + parc + '_' + measure + '_rh.csv'
 
-cmd_l = 'aparcstats2table --hemi lh --subjectsfile {} --skip --meas {} --parc aparc.{}atlas --tablefile {}'.format(subjectfile,measure,parc,file_l)
-cmd_r = 'aparcstats2table --hemi rh --subjectsfile {} --skip --meas {} --parc aparc.{}atlas --tablefile {}'.format(subjectfile,measure,parc,file_r)
+cmd_l = 'aparcstats2table --hemi lh --subjectsfile {} --skip --meas {} --parc aparc.{} --tablefile {}'.format(subjectfile,measure,parc,file_l)
+cmd_r = 'aparcstats2table --hemi rh --subjectsfile {} --skip --meas {} --parc aparc.{} --tablefile {}'.format(subjectfile,measure,parc,file_r)
 
 os.environ['SUBJECTS_DIR'] = subject_dir 
-os.system('module load freesurfer')
+
 print('generating table for left hemisphere')
 os.system(cmd_l)
-print('generating table for right hemisphere')
+print('\ngenerating table for right hemisphere')
 os.system(cmd_r)
 
