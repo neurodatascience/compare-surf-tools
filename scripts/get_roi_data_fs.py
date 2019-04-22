34e8d9145494357b0d9c32d470201b7e04bfbd0e
nikhil153 <nikhil153@gmail.com>
2019-04-22 16:39:38 -0400

    minor changes to comments

--- scripts/get_roi_data_fs.py
+++ scripts/get_roi_data_fs.py
@@ -3,9 +3,7 @@
 # @author Nikhil Bhagawt
 # @date 22 April 2019
 
-#import numpy as np
 import argparse
-import csv
 import os 
 import sys
 
@@ -14,25 +12,27 @@ parser = argparse.ArgumentParser(description='Process ROI-wise output from FS us
 parser.add_argument('-s','--subjectdir',help='path for the file with subject list from freesurfer output')
 parser.add_argument('-l','--listofsubjects',help='path for the file with subject list from freesurfer output')
 parser.add_argument('-m','--meas',help='phenotypic measure (e.g. thickness, surface area etc.')
-parser.add_argument('-p','--parc',help='cortical surface parcellation (e.g. aparc.Glasseratlas')
+parser.add_argument('-p','--parc',help='cortical surface parcellation (e.g. a2009s,Glasseratlas')
 parser.add_argument('-o','--output',help='path for the left and right output CSVs for ROI-wise cortical thickness from freesurfer output')
 
 args = parser.parse_args()
-
 subject_dir = args.subjectdir
 subjectfile = args.listofsubjects
 measure = args.meas
 parc = args.parc
+
+# output files
 file_l = args.output + parc + '_' + measure + '_lh.csv'
 file_r = args.output + parc + '_' + measure + '_rh.csv'
 
+# freesurfer commands
 cmd_l = 'aparcstats2table --hemi lh --subjectsfile {} --skip --meas {} --parc aparc.{} --tablefile {}'.format(subjectfile,measure,parc,file_l)
 cmd_r = 'aparcstats2table --hemi rh --subjectsfile {} --skip --meas {} --parc aparc.{} --tablefile {}'.format(subjectfile,measure,parc,file_r)
 
+# env var required by freesurfer
 os.environ['SUBJECTS_DIR'] = subject_dir 
 
-print('generating table for left hemisphere')
+print('\ngenerating table for left hemisphere')
 os.system(cmd_l)
 print('\ngenerating table for right hemisphere')
 os.system(cmd_r)
-
