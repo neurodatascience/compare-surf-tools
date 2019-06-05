7af908fe204ccd0d56d46fbc42a360a94833f2d2
nikhil153 <nikhil153@gmail.com>
2019-06-05 16:12:21 -0400

    fixed removal of zero vertex column

--- scripts/merge_large_dataframes.py
+++ scripts/merge_large_dataframes.py
@@ -50,15 +50,13 @@ for i in range(n_iter):
     else: 
         data_df = pd.read_csv(vertex_file, header=None, skiprows = skip_rows, nrows = batch_size)
     
-    if drop_condition is not None:
-        print('Dropping columns with all 0s')
-        data_df = data_df.loc[:, (data_df != drop_condition).any(axis=0)]
-
-    print('rows {}:{}'.format(skip_rows,skip_rows+len(data_df)))
-    data_df.columns = [Subject_id_col] + list(range(data_df.shape[1]-1))
+    print('Reading rows {}:{}'.format(skip_rows,skip_rows+len(data_df)))
+    mr_cols = list(range(data_df.shape[1]-1))
+    data_df.columns = [Subject_id_col] + mr_cols
     data_df = pd.merge(data_df,demo_df,on=Subject_id_col,how='left')
     
-    with open(out_csv, 'a') as f:
+    demoMerged_csv = out_csv + 'demoMerged'
+    with open(demoMerged_csv, 'a') as f:
         if i==0:
             data_df.to_csv(f, header=True)
         else:
@@ -67,4 +65,44 @@ for i in range(n_iter):
     del data_df
     skip_rows += batch_size
 
-print('Saving merged CSV here: {}'.format(out_csv))
+if drop_condition is not None:
+    print('Dropping columns with all 0s')
+    
+    # Need to read all rows and few columns to make sure every subject has 0s in that column (vertex)
+
+    col_size = 10000
+    non_zero_cols = []
+    for start_idx in range(0,len(mr_cols),col_size):
+        end_idx = start_idx + col_size
+        print('Reading columns {}:{}'.format(start_idx,end_idx))
+        col_subset = mr_cols[start_idx:end_idx]
+        data_df =pd.read_csv(demoMerged_csv,usecols=col_subset)
+        data_df = data_df.loc[:, (data_df != drop_condition).any(axis=0)]
+        non_zero_cols = non_zero_cols + list(data_df.columns)
+    
+    print("Number of non-zero columns across all subjects: {}".format(len(non_zero_cols)))
+
+    # One you have the list of nonzero columns you can re-read the csv by rows
+    print('Writing csv with dropped columns')
+    skip_rows = 0
+    for i in range(n_iter):
+        if i == n_iter - 1:
+            data_df = pd.read_csv(demoMerged_csv, header=None, skiprows = skip_rows, usecols=non_zero_cols)
+            print('df shape after dropping zeros {}'.format(data_df.shape))
+        else: 
+            data_df = pd.read_csv(demoMerged_csv, header=None, skiprows = skip_rows, nrows = batch_size, usecols=non_zero_cols)
+        
+        print('rows {}:{}'.format(skip_rows,skip_rows+len(data_df)))
+        
+        nonzero_csv = out_csv + '_nonzero.csv'
+        with open(nonzero_csv, 'a') as f:
+            if i==0:
+                data_df.to_csv(f, header=True)
+            else:
+                data_df.to_csv(f, header=False)
+        
+        del data_df
+        skip_rows += batch_size
+
+print('Saving merged CSV here: {}'.format(nonzero_csv))
+
